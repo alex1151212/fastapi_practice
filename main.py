bd1f7f886a1f21a87eb6fe1ac748c4164046101e
alex1151212 <alex1151212@gmail.com>
2022-01-08 02:08:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -186,33 +186,3 @@ def signup_post_view():
     response.delete_cookie("Authentication")
     return response
 
-
-@app.post('/user/',tags=["User"])
-def user_register(request:schemas.User ,db:Session = Depends(get_db)):
-    new_user = models.User(username=request.username,password=Hash.bcrypt(requestpassword))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return {
-        "status":"ok",
-        "data":f"Hello {new_user.username}"
-    }
-
-
-@app.get('/user/',tags=["User"])
-def user_get_all(db:Session = Depends(get_db)):
-    users = db.query(models.User).all()
-    return users
-
-@app.get('/user/{username}',tags=["User"])
-def user_get_by_username(username,db:Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.username == username).first()
-    return user
-
-@app.delete('/user/{username}',tags=["User"])
-def user_delete_by_username(username,db:Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.username == username).delete()
-    db.commit()
-    if user :
-        return "Delete Successfuly!"
-    raise HTTPException(404,"User Not Found!")
